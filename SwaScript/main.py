8d70173a9ccf268cacac14a17d32dfb8a3fae6eb
khalifa47 <khalifafumo5@gmail.com>
2023-10-27 19:06:22 +0300

    clean

--- SwaScript/main.py
+++ SwaScript/main.py
@@ -1,27 +1,3 @@
-# from lexer import lexer
-# from parse import Parser
-# from interpreter import Interpreter
-
-# test_code = """kwa kila i katika 1 hadi 5 fanya
-#                     chapisha i
-#                 mwisho"""
-
-# test_tokens = lexer(test_code)
-# print("Tokens:")
-# print(test_tokens)
-
-# parser = Parser(test_tokens)
-# ast = parser.parse_program()
-# print("AST:")
-# print(ast)
-
-# interpreter = Interpreter()
-# res = interpreter.interpret_program(ast)
-# print("Result:")
-# print(res)
-
-# test_swascript.py
-
 import argparse
 from lexer import lexer
 from parse import Parser
