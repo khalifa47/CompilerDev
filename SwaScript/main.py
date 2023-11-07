fc07803963ebbe3bea74ba27ed250038fcdb78ad
khalifa47 <khalifafumo5@gmail.com>
2023-11-07 23:10:27 +0300

    pretty print

--- SwaScript/main.py
+++ SwaScript/main.py
@@ -2,6 +2,7 @@ import argparse
 from lexer import lexer
 from parse import Parser
 from interpreter import Interpreter
+from pprint import pprint
 
 
 def main():
@@ -16,17 +17,25 @@ def main():
 
     # Tokenize the code
     tokens = lexer(swa_code)
+    print("\n====================================TOKENS====================================\n")
+    pprint(tokens)
+    print("\n==============================================================================\n")
 
     # Parse the tokens into an AST
     parser = Parser(tokens)
     ast = parser.parse_program()
+    print("\n=====================================AST======================================\n")
+    pprint(ast)
+    print("\n==============================================================================\n")
 
     # Interpret the AST
     interpreter = Interpreter()
     res = interpreter.interpret_program(ast)
 
     # Print the result
-    print(res)
+    print("\n====================================RESULT====================================\n")
+    pprint(res)
+    print("\n==============================================================================\n")
 
 
 if __name__ == "__main__":
